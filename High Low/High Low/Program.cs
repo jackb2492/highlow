933f4a31aaa2f8e2e7e2a51c7e6cc96194d87ab3
jackb2492 <71940178+jackb2492@users.noreply.github.com>
2020-10-05 13:39:43 +0100

    Add files via upload

---
+++ High Low/High Low/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace High_Low
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome message
+
+            Console.WriteLine("Welcome to the High Low game");
+
+            // Choose random number
+            Random rng = new Random();
+            int number = rng.Next(1, 11);
+            int tries = 0;
+
+            while (true)
+            {
+
+                // Enter user guess
+                Console.WriteLine("Enter guess :"); 
+                int guess = Convert.ToInt32(Console.ReadLine());
+                tries = tries + 1;
+                
+               
+
+            
+                // Compare guess to number
+                if (guess < number)
+                {
+                    Console.WriteLine("Too low, go higher.");
+
+
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Too high, go lower");
+                }
+                else
+                {
+                    Console.WriteLine("Correct well done.");
+                    Console.WriteLine(tries.ToString()+ " guesses");
+                    break;
+                }
+            }
+            
+            // Wait for a key press
+
+            Console.ReadKey();
+        }
+    }
+}
